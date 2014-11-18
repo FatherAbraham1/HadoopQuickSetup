54e2944a8ba6c960027e5f7aa07c3857b15f9f01
javachen <june.chan@foxmail.com>
2014-11-18 11:17:00 +0800

    update

--- shell/format_cluster.sh
+++ shell/format_cluster.sh
@@ -21,17 +21,18 @@ su -s /bin/bash hdfs -c 'hadoop fs -chmod 755 /'
 
 while read dir user group perm
 do
-   	su -s /bin/bash hdfs -c "hadoop fs -mkdir -p $dir && hadoop fs -chmod $perm $dir && hadoop fs -chown $user:$group $dir"
+   	su -s /bin/bash hdfs -c "hadoop fs -mkdir -p $dir ; hadoop fs -chmod -R $perm $dir ; hadoop fs -chown $user:$group $dir"
     	echo "[INFO]:mkdir $dir"
 done << EOF
-/hbase hbase hadoop 755
 /tmp hdfs hadoop 1777 
 /yarn/apps yarn mapred 1777
 /user hdfs hadoop 777
 /user/root root hadoop 755
-/user/hive hive hadoop 775
+/user/hive hive hadoop 777
 /user/hive/warehouse hive hadoop 1777
 /user/history yarn hadoop 1777
 /user/history/done mapred hadoop 750
 /user/history/done_intermediate mapred hadoop 1777 
+/hbase hbase hadoop 755
+
 EOF
